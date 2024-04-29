6c1f70562a6e3fb5234e19b86ee3eb0769d4e87d
mclaramarinho <marinho.claramb@gmail.com>
2024-04-29 20:02:36 -0300

    feat: created an endpoint to get substances and commercial names by name

--- router/substancesAndNames.py
+++ router/substancesAndNames.py
@@ -12,6 +12,20 @@ from router.utils.pagination import pagination, handle_500_response
 
 router = APIRouter()
 
+@router.get(path="/getByName", responses=get_response_doc('subst'))
+async def get_by_name(req: Request, name: str, count: int = None, page: int = None, api_key: str = Depends(get_api_key)):
+    collection = DBCollection(req.url.path).collection
+    r = pagination(count, page, collection, filter={'name': {'$regex': name}})
+
+    if r.code == 200:
+        return GetAllSubstancesRes(content=r.content, totalElements=r.total_elements, elementsPerPage=r.items_per_page,
+                                   currentPage=r.current_page, totalPages=r.total_pages, firstPage=r.first_page,
+                                   lastPage=r.last_page)
+
+    else:
+        return JSONResponse(status_code=r.code, content={'message': r.message})
+
+
 
 @router.get(path="/all", responses=get_response_doc('subst'))
 async def get_all(req: Request, count: int = None, page: int = None, api_key: str = Depends(get_api_key)):
